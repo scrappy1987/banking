120f1e888584cc010332cada670433acb3dd2c13
scrappy1987 <jagordon87@msn.com>
2018-11-13 09:48:46 +0000

    Third commit

--- src/main/java/com/qa/app/App.java
+++ src/main/java/com/qa/app/App.java
@@ -1,9 +1,14 @@
 package com.qa.app;
 
+import com.qa.domain.Person;
+
 public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello banking");
+		Person john = new Person();
+		john.setFirstName("John");
+		System.out.println("The name of the customer is " + john.getFirstName());
 
 	}
 
